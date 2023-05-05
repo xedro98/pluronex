19c3386b569e60d1d5572bca44fff83070ea4eda
Shubhankar Kahali <shubhankar.kahali20@ibsindia.org>
2023-05-05 21:42:31 +0530

    hkjk

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,6 +1,6 @@
 # Contributing Guidelines
 
-**Welcome to Chatbot UI!**
+**Welcome to Pluro Go!**
 
 We appreciate your interest in contributing to our project.
 
--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -396,7 +396,7 @@ export const ChatInput = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         
-        .{' '}
+        {' '}
         {t(
           "Pluro Go may produce inaccurate information about people, places, or facts.",
         )}
